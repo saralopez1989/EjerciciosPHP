499cafd0c733586c9131fb29317d071db38c7b20
saralopez1989 <65974192+saralopez1989@users.noreply.github.com>
2020-05-30 21:29:42 -0600

    Update Ejercicio_2.php

--- Ejercicio_2.php
+++ Ejercicio_2.php
@@ -19,7 +19,7 @@
     echo "<html><head><meta charset=\"utf-8\"></head>";
     echo "<body>";
     echo "<h2>Esta es la clase de $NombreClase mañana y tarde (Símbolo) </h2>";
-    echo "<h2>Esta es otra fila de la clase de ".$NombreClase."con dos cadenas concatenadas </h2>";
+    echo "<h2>Esta es otra fila de la clase de ".$NombreClase." con dos cadenas concatenadas </h2>";
     echo "</body>";
     echo "</html>";
 
