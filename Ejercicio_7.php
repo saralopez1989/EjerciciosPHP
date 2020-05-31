0fb64976ee898c940b686dc953b10e28e99f5834
saralopez1989 <65974192+saralopez1989@users.noreply.github.com>
2020-05-31 14:00:57 -0600

    Update Ejercicio_7.php

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -7,21 +7,28 @@
 <?php
 header ("Content-type: text/html;charset=\"utf-8\"");
 
-
-    if (is_numeric($num) && $num >1)
+    if (is_numeric($_GET['numero']) && $_GET['numero'] >1)
     {
-        echo "<br>El número ".$num." es numerico";
+        echo "<h3>El número ".$_GET['nombre']." es numerico</h3>";
     }
     else
     {
-        echo "<br>El número ".$num." NO es numerico";
+        echo "<h3>El número ".$_GET['nombre']." No es numerico</h3>";
     }
 
-
+    if ($_GET['numero']  / 2 != 0) 
+        {
+            echo "<h3>El número ".$_GET['nombre']." es primo</h3>";
+        }
+        else
+        {
+            echo "<h2>El número ".$_GET['nombre']." No es primo</h2>";
+        }
+    
 ?>
 <form>
     Escribe un número:
-    <input name="num" type="text" placeholder="Escribe número">
+    <input name="numero" type="text" placeholder="Escribe número">
     <input type="submit" value="Comprobar">
 </form>
 
