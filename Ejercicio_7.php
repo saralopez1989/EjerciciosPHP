c057c6cb21201ff2230377a2d21811609cd880a7
saralopez1989 <65974192+saralopez1989@users.noreply.github.com>
2020-06-03 10:25:58 -0600

    Update Ejercicio_7.php

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -7,7 +7,7 @@
 <?php
 header ("Content-type: text/html;charset=\"utf-8\"");
 
-    $_GET['numero'] = 0;
+    $_GET['numero'] = 2;
     if(is_int($_GET['numero']) && is_numeric($_GET['numero']) && $_GET['numero'] >1)
     {
         $cont=0;
